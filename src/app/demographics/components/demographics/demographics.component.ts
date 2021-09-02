247319b14660a8a0136adb14b22df792b265f2f9
swati123mukhija <smakhija257@gmail.com>
2021-09-02 17:22:13 +0530

    mobile responsive and validation done

--- src/app/demographics/components/demographics/demographics.component.ts
+++ src/app/demographics/components/demographics/demographics.component.ts
@@ -23,15 +23,21 @@ export class DemographicsComponent implements OnInit {
         Validators.minLength(10),
       ]),
       lastname: new FormControl('', [
-        Validators.required,
-        Validators.minLength(10),
+        Validators.maxLength(15),
+        Validators.pattern('^[a-zA-Z]+$'),
       ]),
       gender: new FormControl('', [Validators.required]),
       ethnicity: new FormControl('', [Validators.required]),
       education: new FormControl('', [Validators.required]),
       occupation: new FormControl('', [Validators.required]),
       address: new FormControl('', [Validators.required]),
-      phone: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+        Validators.minLength(10),
+        Validators.maxLength(10),
+      ]),
+
       medical: new FormControl('', [Validators.required]),
       familymedical: new FormControl('', [Validators.required]),
       surgeries: new FormControl('', [Validators.required]),
@@ -39,6 +45,14 @@ export class DemographicsComponent implements OnInit {
     });
   }
 
+  //only number will be add
+  keyPress(event: any) {
+    const pattern = /[0-9\+\-\ ]/;
+    let inputChar = String.fromCharCode(event.charCode);
+    if (event.keyCode != 8 && !pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.demoForm.value);
